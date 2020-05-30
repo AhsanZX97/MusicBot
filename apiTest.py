b5c331a5f23a503b39afab71c9943b4dc54b33c4
Ahsan Zia <ahsan97@hotmail.co.uk>
2020-05-30 15:34:34 +0100

    can make a playlist and add a song

--- apiTest.py
+++ apiTest.py
@@ -9,9 +9,10 @@ import pprint
 #client_credentials_manager = SpotifyClientCredentials(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET,)
 #token = client_credentials_manager.get_access_token()
 
-sp = spotipy.Spotify(auth="BQCrvPkmx6EHjdPr8Rncct4DR1S7IowvDoQqtcg47Mw7ScmYdoVYDi")
-
-print(sp.search(q='offset', limit=1,type='artist'))
+token = util.prompt_for_user_token("kingpiccy","playlist-modify-public playlist-modify-private",client_id=SPOTIPY_CLIENT_ID,client_secret=SPOTIPY_CLIENT_SECRET,redirect_uri='http://localhost:8080/') 
+sp = spotipy.Spotify(auth=token)
+track = sp.search(q='baby powder jenevieve', limit=1,type='track')
+track_uri = [ track['tracks']['items'][0]['uri'] ]
 
 #sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
@@ -23,4 +24,6 @@ print(sp.search(q='offset', limit=1,type='artist'))
 
 
 
-#sp.user_playlist_create(user="kingpiccy",name="Test playlist",)
+playlist = sp.user_playlist_create(user="kingpiccy",name="Test playlist")
+
+sp.user_playlist_add_tracks("kingpiccy", playlist['id'], track_uri)
