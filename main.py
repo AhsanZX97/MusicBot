5d174e13edf4266f65f6a092e9e78bf3b4b94c1b
Ahsan Zia <ahsan97@hotmail.co.uk>
2020-05-30 18:42:54 +0100

    discord bot now create playlist

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import sys
-from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy
+import spotipy.util as util
 from apikey import SPOTIPY_CLIENT_ID,SPOTIPY_CLIENT_SECRET,DISCORD_TOKEN
 import discord
 
 client = discord.Client()
+
+global sp, token, playlist
+
+token = util.prompt_for_user_token("kingpiccy","playlist-modify-public playlist-modify-private",client_id=SPOTIPY_CLIENT_ID,client_secret=SPOTIPY_CLIENT_SECRET,redirect_uri='http://localhost:8080/') 
+sp = spotipy.Spotify(auth=token)
 
 @client.event
 async def on_ready():
@@ -17,6 +22,14 @@ async def on_message(message):
 
     if message.content.startswith('?create '):
         msg = message.content[8:]
-        await message.channel.send(msg)
+        if len(msg) > 50:
+            await message.channel.send("playlist name needs to be less than 50 characters")
+        else:
+            playlist = sp.user_playlist_create(user="kingpiccy",name=msg,public=True)
+            await message.channel.send("Playlist created: " + playlist['external_urls']['spotify'])
+    if message.content.startswith('?id '):
+        await message.channel.send(playlist['id'])
+
+        
 
 client.run(DISCORD_TOKEN)
