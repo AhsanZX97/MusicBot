4121c3ba42cc8eb70b2bf932f8e4d44b98d475db
Ahsan Zia <ahsan97@hotmail.co.uk>
2020-06-01 22:34:37 +0100

    error handling

--- main.py
+++ main.py
@@ -25,16 +25,36 @@ async def on_message(message):
         return
 
     if message.content.startswith('?create '):
-        msg = message.content[8:]
-        if len(msg) > 50:
-            await message.channel.send("playlist name needs to be less than 50 characters")
+        if playlist is None:
+            msg = message.content[8:]
+            if len(msg) > 50:
+                await message.channel.send("playlist name needs to be less than 50 characters")
+            else:
+                playlist = sp.user_playlist_create(user="kingpiccy",name=msg,public=True)
+                await message.channel.send("Playlist created: " + playlist['external_urls']['spotify'])
+                await message.channel.send("type ?end to end this playlist adding session")
         else:
-            playlist = sp.user_playlist_create(user="kingpiccy",name=msg,public=True)
-            await message.channel.send("Playlist created: " + playlist['external_urls']['spotify'])
+            await message.channel.send("There is already a playlist in session")
     if message.content.startswith('?add '):
-        msg = message.content[5:]
-        track = sp.search(q=msg, limit=1,type='track')
-        track_uri = [ track['tracks']['items'][0]['uri'] ]
-        sp.user_playlist_add_tracks("kingpiccy", playlist['id'], track_uri)
+        if playlist is None:
+            await message.channel.send("No playlist in session")
+        else:
+            msg = message.content[5:]
+            track = sp.search(q=msg, limit=1,type='track')
+            track_uri = [ track['tracks']['items'][0]['uri'] ]
+            sp.user_playlist_add_tracks("kingpiccy", playlist['id'], track_uri)
+            await message.channel.send("Song added: " + track['tracks']['items'][0]['external_urls']['spotify'])
+    if message.content == '?end':
+        await message.channel.send("Playlist adding session has been ended. Here is the final playlist: " + playlist['external_urls']['spotify'])
+        playlist = None
+    if message.content.startswith('?remove '):
+        if playlist is None:
+            await message.channel.send("No playlist in session")
+        else:
+            msg = message.content[8:]
+            track = sp.search(q=msg, limit=1,type='track')
+            track_uri = [ track['tracks']['items'][0]['uri'] ]
+            sp.user_playlist_remove_specific_occurrences_of_tracks("kingpiccy",playlist['id'],track_uri)
+            await message.channel.send("Song removed")
 
 client.run(DISCORD_TOKEN)
