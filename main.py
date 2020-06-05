865669d0d4c5dcccfba678754638ce5492cf06ae
Ahsan Zia <ahsan97@hotmail.co.uk>
2020-06-05 18:56:33 +0100

    mongodb test

--- main.py
+++ main.py
@@ -1,28 +1,42 @@
 import sys
 import spotipy
 import spotipy.util as util
-from apikey import SPOTIPY_CLIENT_ID,SPOTIPY_CLIENT_SECRET,DISCORD_TOKEN
+from apikey import SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, DISCORD_TOKEN
 import discord
+from db import db
 
 client = discord.Client()
 
 global sp, token, playlist
 
-token = util.prompt_for_user_token("kingpiccy","playlist-modify-public playlist-modify-private",client_id=SPOTIPY_CLIENT_ID,client_secret=SPOTIPY_CLIENT_SECRET,redirect_uri='http://localhost:8080/') 
+token = util.prompt_for_user_token("kingpiccy", "playlist-modify-public playlist-modify-private",
+                                   client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri='http://localhost:8080/')
 sp = spotipy.Spotify(auth=token)
 
 playlist = None
+
+history = []
+
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
+
 @client.event
 async def on_message(message):
-    global playlist
+    global playlist, history
 
     if message.author == client.user:
         return
+
+    if message.content == '?test':
+        posts = db.posts
+        post = {"author": "Mike",
+                "text": "My first blog post!",
+                "tags": ["mongodb", "python", "pymongo"]}
+        post_id = posts.insert_one(post).inserted_id
+        await message.channel.send(post_id)
 
     if message.content.startswith('?create '):
         if playlist is None:
@@ -30,7 +44,9 @@ async def on_message(message):
             if len(msg) > 50:
                 await message.channel.send("playlist name needs to be less than 50 characters")
             else:
-                playlist = sp.user_playlist_create(user="kingpiccy",name=msg,public=True)
+                playlist = sp.user_playlist_create(
+                    user="kingpiccy", name=msg, public=True)
+                history.append({'id': playlist['id']})
                 await message.channel.send("Playlist created: " + playlist['external_urls']['spotify'])
                 await message.channel.send("type ?end to end this playlist adding session")
         else:
@@ -40,13 +56,15 @@ async def on_message(message):
             await message.channel.send("No playlist in session")
         else:
             msg = message.content[5:].strip()
-            track = sp.search(q=msg, limit=1,type='track')
+            track = sp.search(q=msg, limit=1, type='track')
             if len(track['tracks']['items']) == 0:
                 await message.channel.send("Song not found, learn to type you bozo")
             else:
-                track_uri = [ track['tracks']['items'][0]['uri'] ]
-                sp.user_playlist_add_tracks("kingpiccy", playlist['id'], track_uri)
+                track_uri = [track['tracks']['items'][0]['uri']]
+                sp.user_playlist_add_tracks(
+                    "kingpiccy", playlist['id'], track_uri)
                 await message.channel.send("Song added: " + track['tracks']['items'][0]['external_urls']['spotify'])
+
     if message.content == '?end':
         await message.channel.send("Playlist adding session has been ended. Here is the final playlist: " + playlist['external_urls']['spotify'])
         playlist = None
@@ -55,16 +73,17 @@ async def on_message(message):
             await message.channel.send("No playlist in session")
         else:
             msg = message.content[8:]
-            track = sp.search(q=msg, limit=1,type='track')
+            track = sp.search(q=msg, limit=1, type='track')
             if len(track['tracks']['items']) == 0:
                 await message.channel.send("Song not found")
             else:
-                track_uri = [ track['tracks']['items'][0]['uri'] ]
-                sp.user_playlist_remove_all_occurrences_of_tracks("kingpiccy",playlist['id'],track_uri)
+                track_uri = [track['tracks']['items'][0]['uri']]
+                sp.user_playlist_remove_all_occurrences_of_tracks(
+                    "kingpiccy", playlist['id'], track_uri)
                 await message.channel.send("Song removed")
     if message.content.startswith('?view '):
         msg = message.content[6:].strip()
-        playlists = sp.current_user_playlists(limit = 50, offset=0)['items']
+        playlists = sp.current_user_playlists(limit=50, offset=0)['items']
         found = False
         for p in playlists:
             if p['name'] == msg:
@@ -79,7 +98,7 @@ async def on_message(message):
             await message.channel.send("Current playlist in session: " + playlist['external_urls']['spotify'])
     if message.content.startswith('?modify '):
         msg = message.content[8:].strip()
-        playlists = sp.current_user_playlists(limit = 50, offset=0)['items']
+        playlists = sp.current_user_playlists(limit=50, offset=0)['items']
         found = False
         for p in playlists:
             if p['name'] == msg:
