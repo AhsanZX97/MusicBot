b6b13ace68061c9fb9061331b7d0792838203056
Ahsan Zia <ahsan97@hotmail.co.uk>
2020-06-10 21:05:11 +0100

    started work on embed function

---
+++ 2ndmain.py
@@ -0,0 +1,33 @@
+import sys
+import spotipy
+import spotipy.util as util
+from apikey import SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, DISCORD_TOKEN, USERNAME
+import discord
+from db import db
+
+client = discord.Client()
+
+global sp, token, playlist
+
+token = util.prompt_for_user_token("kingpiccy", "playlist-modify-public playlist-modify-private",client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri='http://localhost:8080/')
+sp = spotipy.Spotify(auth=token)
+
+playlist = None
+total = 0
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    global playlist, total
+
+    if message.author == client.user:
+        return
+
+    if "music.apple.com" in message.content:
+        await message.channel.send(message.embeds[0])
+
+client.run(DISCORD_TOKEN)
